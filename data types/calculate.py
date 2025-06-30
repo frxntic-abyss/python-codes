9a67d6d817a62154a54832c75ae84aa11fb8a7ae
frxntic-abyss <shekhargyanvi@gmail.com>
2025-06-30 21:00:59 +1000

    commit

---
+++ data types/calculate.py
@@ -0,0 +1,22 @@
+num1 = 45
+num2 = 3
+
+
+print("Number 1", num1)
+print("Number 2", num2)
+print("Addition :", num1+num2)
+print("SUbtraction :", num1-num2)
+print("Multiplication :", num1*num2)
+print("Division :", num1/num2)
+print("floor division :", num1//num2)
+print("Modulus Division :", num1%num2)
+print("Square :", num2**2)
+print("Square Root :", num1**0.5)
+
+print("Equal ?", num1==num2)
+print("Number 1 greater?", num1>num2)
+print("Number 2 greater?", num1<num2)
+print("Not equal?", num1!=num2)
+
+result = num1/2+num2**2+10
+print("Result of the given equation is: ", result)
